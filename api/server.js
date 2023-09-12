6509a3ecf806e588bbf64f568ac37e7e1e32ce0d
Ali Omar Abdi <Mafiobio700@gmail.com>
2023-09-12 21:08:12 +0300

    Server

--- api/server.js
+++ api/server.js
@@ -1,11 +1,13 @@
-import express, { json } from "express";
-// import authorsRoute from "./authors.js";
-import route from "./authors.js";
-
+import express from "express";
+import authorsRoute from "./authors.js";
+import booksRoute from "./books.js";
+import bookStoreRouter from "./bookstores.js";
 const server = express();
 
 server.use(express.json());
 
-server.use("/api/authors", route);
+server.use("/api/authors", authorsRoute);
+server.use("/api/books", booksRoute);
+server.use("/api/bookstores", bookStoreRouter);
 
 export default server;
