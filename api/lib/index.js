be58ad255ed6dc8c120d5f71b3076fc19ca834a7
Ali Omar Abdi <Mafiobio700@gmail.com>
2023-09-14 19:00:27 +0300

    last update

--- api/lib/index.js
+++ api/lib/index.js
@@ -1,7 +1,7 @@
 // Setup Prisma Client
 
-import { PrismaClient} from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const Prisma = new PrismaClient();
 
-export default prisma;
+export default Prisma;
